e5a4ccb2dc12b603d09fe5b706cc250e433c5093
diegomanga26 <diegoalexandermanga@gmail.com>
2023-06-28 16:52:36 -0500

    feat: :sparkles: Crud completo de "departamento"
    Se completó el crud de la tabla "departamento", con las 4 peticiones requeridas.

--- uploads/app.php
+++ uploads/app.php
@@ -18,7 +18,7 @@
         $_DATA = json_decode(file_get_contents("php://input"), true);
         $cox = new \App\connect();
         $res = $cox->con->prepare("UPDATE pais SET nombrePais = :NOMBRE_PAIS WHERE idPais =:ID");
-        $res-> bindValue("NOMBRE_PAIS", $_DATA['nombrePais']); //para editar se debe escribir la sentencia dentro del $_DATA["nom"] es decir { nom: Wilfer, id: 1}
+        $res-> bindValue("NOMBRE_PAIS", $_DATA['nombrePais']);
         $res-> bindValue("ID", $_DATA['idPais']);
         $res -> execute();
         $res = $res->rowCount();
@@ -44,6 +44,53 @@
         $res = $res->rowCount();
         echo json_encode($res);
     });
+
+    /**
+     * ! CRUD DE LA TABLA "departamento":
+     */
+
+    $router->get("/dep", function(){
+        $cox = new \App\connect();
+        $res = $cox->con->prepare("SELECT * FROM departamento");
+        $res -> execute();
+        $res = $res->fetchAll(\PDO::FETCH_ASSOC);
+         // retorna la consulta como un array asociativo 
+        echo json_encode($res);
+    });
+
+    $router->put("/dep", function(){
+        $_DATA = json_decode(file_get_contents("php://input"), true);
+        $cox = new \App\connect();
+        $res = $cox->con->prepare("UPDATE departamento SET nombreDep = :NOMBRE_DEP, idPais = :IDPAIS WHERE idDep =:ID");
+        $res-> bindValue("NOMBRE_DEP", $_DATA['nombreDep']);
+        $res-> bindValue("IDPAIS", $_DATA['idPais']);
+        $res-> bindValue("ID", $_DATA['idDep']);
+        $res -> execute();
+        $res = $res->rowCount();
+        echo json_encode($res);
+    });
+
+    $router -> delete("/dep", function(){
+        $_DATA = json_decode(file_get_contents("php://input"), true);
+        $cox = new \App\connect();
+        $res = $cox->con->prepare("DELETE FROM departamento WHERE idDep =:ID");
+        $res->bindValue("ID", $_DATA["idDep"]);
+        $res->execute();
+        $res = $res->rowCount();
+        echo json_encode($res);
+    });
+
+    $router->post("/dep", function(){
+        $_DATA = json_decode(file_get_contents("php://input"), true);
+        $cox = new \App\connect();
+        $res = $cox->con->prepare("INSERT INTO departamento (nombreDep, idPais) VALUES (:NOMBRE_DEP, :IDPAIS)");
+        $res-> bindValue("NOMBRE_DEP", $_DATA['nombreDep']); 
+        $res-> bindValue("IDPAIS", $_DATA['idPais']);
+        $res -> execute();
+        $res = $res->rowCount();
+        echo json_encode($res);
+    });
+    
 
     $router->run();
     /*
